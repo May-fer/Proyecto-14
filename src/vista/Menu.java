552ef2938a34a0c7caf124a9fb58f568cdba96bd
TomásC <tcoba2006@gmail.com>
2026-05-24 23:48:22 -0500

    Avance menu y main

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -1,5 +1,35 @@
 package vista;
-
+import java.util.Scanner;
 public class Menu {
+    private Scanner scanner = new Scanner(System.in);
 
+    public void mostrarMenu() {
+        int key;
+        do{
+        System.out.println("=== Menú ===");
+        
+        System.out.println("1. Comparar Kruskal y Prim");
+        System.out.println("2. Mostrar tabla comparativa");
+        System.out.println("0. Salir");
+        System.out.print("Seleccione una opción: ");
+
+        key = scanner.nextInt();
+
+    }while (key != 0);
+
+    }
+    
+    private void opciones(int key){
+        switch (key) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default: System.out.println("Opción no válida.");
+                break;
+    }
 }
+}
+
